a771c0a73eeb20f32ea9d2bc2ea71b0beaaa61a7
CarolineMVB <92635955+CarolineMVB@users.noreply.github.com>
2022-02-07 09:55:22 +0100

    creation version.php

--- version.php
+++ version.php
@@ -1,9 +1,10 @@
 <?php
+// version.php
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version    = 2022020202;
-$plugin->requires   = 2016052300;
-$plugin->release    = '1.0.0';
-$plugin->component  = 'block_tutorial_block';
-$plugin->maturity   = MATURITY_STABLE;
+$plugin->version = 2022020200;
+$plugin->requires = 2016052300;
+$plugin->release = '1.0.0';
+$plugin->component = 'block_tutorial_block';
+$plugin->maturity = MATURITY_STABLE;
